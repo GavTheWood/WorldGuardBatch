88bac301bfd924ccc72017e09c99623ef71a44d0
Florian Fülling <mail@code-fox.de>
2019-06-27 08:49:22 +0200

    add help command

--- src/main/java/de.eldoria.worldguardbatch/commands/BaseCommand.java
+++ src/main/java/de.eldoria.worldguardbatch/commands/BaseCommand.java
@@ -1,5 +1,6 @@
 package de.eldoria.worldguardbatch.commands;
 
+import de.eldoria.worldguardbatch.Messages;
 import de.eldoria.worldguardbatch.RegionLoader;
 import de.eldoria.worldguardbatch.commands.subcommands.*;
 import org.bukkit.command.Command;
@@ -7,13 +8,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.StringJoiner;
+
 public class BaseCommand implements CommandExecutor {
 
     private MembershipManager membershipManager;
     private PriorityManager priorityManager;
     private ParentManager parentManager;
     private FlagManager flagManager;
-    private CheckSubCommand checkSubCommand;
+    private CheckSubcommand checkSubCommand;
 
     /**
      * Creates a new Base Command Object.
@@ -25,7 +28,7 @@ public class BaseCommand implements CommandExecutor {
         this.priorityManager = new PriorityManager(regionLoader);
         this.parentManager = new ParentManager(regionLoader);
         this.flagManager = new FlagManager(regionLoader);
-        this.checkSubCommand = new CheckSubCommand(regionLoader);
+        this.checkSubCommand = new CheckSubcommand(regionLoader);
     }
 
     @Override
@@ -41,7 +44,10 @@ public class BaseCommand implements CommandExecutor {
             //TODO: Info command
         }
         if (args.length == 1) {
-            //TODO help
+            if (args[0].equalsIgnoreCase("help")) {
+                executeHelpCommand(p);
+                return true;
+            }
         }
         if (args.length > 1) {
             var primaryArg = PrimaryActionArgument.getPrimary(args[0]);
@@ -62,11 +68,11 @@ public class BaseCommand implements CommandExecutor {
                 case PSET:
                 case CREM:
                 case PREM:
-                    parentManager.directCommand(p,args);
+                    parentManager.directCommand(p, args);
                     break;
                 case FSET:
                 case FREM:
-                    flagManager.directCommand(p,args);
+                    flagManager.directCommand(p, args);
                     break;
                 case CHECK:
                 case LIST:
@@ -79,6 +85,19 @@ public class BaseCommand implements CommandExecutor {
             }
         }
 
-        return false;
+        return true;
+    }
+
+    private void executeHelpCommand(Player p) {
+        StringJoiner stringJoiner = new StringJoiner("\n");
+        
+        for (PrimaryActionArgument arg : PrimaryActionArgument.values()) {
+            Messages.CommandText cmdText = Messages.getCommandText(arg);
+            if (cmdText != null) {
+                stringJoiner.add(cmdText.getDescription()).add(cmdText.getPattern());
+            }
+        }
+
+        p.sendMessage(stringJoiner.toString());
     }
 }
